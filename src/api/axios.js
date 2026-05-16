229ee3f348a5d0e4029b7a54b687dc3a444ae5ad
erankit011 <kumarankit97065@gmail.com>
2026-05-16 13:43:00 +0530

    update:fix axios file

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api/v1',
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true, // Enable cookies for refresh tokens
   headers: {
     'Content-Type': 'application/json',
