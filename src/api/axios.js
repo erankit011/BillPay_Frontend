8faa3466f237e966ead32e74429a849703184341
erankit011 <erankit011@gmail.com>
2026-09-23 21:57:12 +0530

    ngrok changes

--- src/api/axios.js
+++ src/api/axios.js
@@ -5,8 +5,6 @@ const api = axios.create({
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': 'https://billpay-frontend-wk6i.onrender.com',
-    'Access-Control-Allow-Credentials': 'true',
     "ngrok-skip-browser-warning": "true",
   },
 });
